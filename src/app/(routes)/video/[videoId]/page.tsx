677b033ed148ed6d907e49b82da3f5041b5192c6
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-03-03 21:01:45 +0100

    Fix import path for LikeSubscribeSection component in Video page

--- src/app/(routes)/video/[videoId]/page.tsx
+++ src/app/(routes)/video/[videoId]/page.tsx
@@ -6,7 +6,7 @@ import getCommentsByVideoId from "@/actions/getCommentsByVideoId";
 import { getRecommendedVideos } from "@/actions/getRecommendedVideos";
 import increaseVideoViewCount from "@/actions/increaseVideoViewCount";
 import CommentSection from "@/components/video/CommentSection/CommentSection";
-import LikeSubscribeSection from "@/components/video/likeSubscribeSection/LikeSubscribeSection";
+import LikeSubscribeSection from "@/components/video/LikeSubscribeSection/LikeSubscribeSection";
 
 interface VideoPageParams {
   videoId?: string;
